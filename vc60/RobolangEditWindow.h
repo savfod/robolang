f7bb846d055132384ca2921d178fabefcc81dd15
SAVsmail <SAVsmail@11fe8b10-5191-11de-bc8f-bda5044519d4>
2009-06-28 16:31:39 +0000

    initial upload

---
+++ vc60/RobolangEditWindow.h
@@ -0,0 +1,49 @@
+#if !defined(AFX_ROBOLANGEDITWINDOW_H__5848CC7C_CCB0_4541_93B6_9DD6140B7F5C__INCLUDED_)
+#define AFX_ROBOLANGEDITWINDOW_H__5848CC7C_CCB0_4541_93B6_9DD6140B7F5C__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// RobolangEditWindow.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CRobolangEditWindow window
+
+class CRobolangEditWindow : public CListView
+{
+// Construction
+public:
+	CRobolangEditWindow();
+	DECLARE_DYNCREATE( CRobolangEditWindow );
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CRobolangEditWindow)
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CRobolangEditWindow();
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CRobolangEditWindow)
+		// NOTE - the ClassWizard will add and remove member functions here.
+	//}}AFX_MSG
+
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_ROBOLANGEDITWINDOW_H__5848CC7C_CCB0_4541_93B6_9DD6140B7F5C__INCLUDED_)
---
+++ vc60/RobolangMap.cpp
@@ -0,0 +1,33 @@
+// RobolangMap.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "robolang.h"
+#include "RobolangMap.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CRobolangMap
+IMPLEMENT_DYNCREATE( CRobolangMap , CWnd )
+
+CRobolangMap::CRobolangMap()
+{
+}
+
+CRobolangMap::~CRobolangMap()
+{
+}
+
+BEGIN_MESSAGE_MAP(CRobolangMap, CWnd)
+	//{{AFX_MSG_MAP(CRobolangMap)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CRobolangMap message handlers
