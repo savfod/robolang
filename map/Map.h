8f297ec82e5093ffd3f68262269cb05507a5e6ef
SAVsmail <SAVsmail@11fe8b10-5191-11de-bc8f-bda5044519d4>
2009-06-30 16:31:52 +0000

    Добавлены пустые классы для подсистем (Map, MapUI, Program, ProgramUI, Interpreter) Некоторые файлы, изменяемые при каждой компиляции, помечены как игнорируемые.

---
+++ map/Map.h
@@ -0,0 +1,20 @@
+// Map.h: interface for the CMap class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_MAP_H__99A05FF9_B630_4808_A98C_3459904AD4E6__INCLUDED_)
+#define AFX_MAP_H__99A05FF9_B630_4808_A98C_3459904AD4E6__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CMap  
+{
+public:
+	CMap();
+	virtual ~CMap();
+
+};
+
+#endif // !defined(AFX_MAP_H__99A05FF9_B630_4808_A98C_3459904AD4E6__INCLUDED_)
---
+++ mapui/MapUI.h
@@ -0,0 +1,20 @@
+// MapUI.h: interface for the CMapUI class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_MAPUI_H__0B3F765C_1712_4B73_BB42_05887E40ADBE__INCLUDED_)
+#define AFX_MAPUI_H__0B3F765C_1712_4B73_BB42_05887E40ADBE__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CMapUI  
+{
+public:
+	CMapUI();
+	virtual ~CMapUI();
+
+};
+
+#endif // !defined(AFX_MAPUI_H__0B3F765C_1712_4B73_BB42_05887E40ADBE__INCLUDED_)
